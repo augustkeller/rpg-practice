37fa7abaf6239560b5785d5b10f139c2d6e07198
augustkeller <guskeller@gmail.com>
2026-03-18 12:12:50 -0500

    antagonistHealth added, targeting aspect started in functions

--- rpg.js
+++ rpg.js
@@ -1,11 +1,12 @@
 let protagonistHealth = 50;
+let antagonistHealth = 50;
 
-function fireball (damage){
+function fireball (damage, target){
     protagonistHealth -= damage;
     return `You deal ${damage} damage!`;
 }
 
-function heal (health){
+function heal (health, target){
     protagonistHealth += health
     return `You heal ${health} life!`;
 }
@@ -14,3 +15,4 @@ fireball(5);
 heal(100);
 
 console.log(protagonistHealth);
+console.log(antagonistHealth);
