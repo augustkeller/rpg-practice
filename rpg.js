49fb148537b5e9ba5e762ef717fc01c32b07263b
augustkeller <guskeller@gmail.com>
2026-03-18 12:05:39 -0500

    heal function created

--- rpg.js
+++ rpg.js
@@ -1,5 +1,11 @@
 function fireball (damage){
-    return `You deal ${damage} damage!`
+    return `You deal ${damage} damage!`;
 }
 
-console.log(fireball(5))
+console.log(fireball(5));
+
+function heal (health){
+    return `You heal ${health} life!`;
+}
+
+console.log(heal(5));
