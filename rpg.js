b24dc86d89c64000f9fd12b6889bf875fb8964be
augustkeller <guskeller@gmail.com>
2026-03-18 12:29:12 -0500

    CharacterAction class started to replace individual functions

--- rpg.js
+++ rpg.js
@@ -12,6 +12,10 @@ class Character {
 let protagonist = new Character("Protagonist", 50);
 let antagonist = new Character("Antagonist", 50);
 
+class CharacterAction {
+    //replace fireball and heal functions here
+}
+
 function fireball(amount, target) {
   target.health -= amount;
   return `You deal ${amount} damage!`;
