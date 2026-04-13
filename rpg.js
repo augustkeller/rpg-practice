f9b8302a80ab27b9611bd2351fcbaad5ac5c56f4
augustkeller <guskeller@gmail.com>
2026-04-13 12:21:43 -0500

    let keyword added to amount variables in functions

--- rpg.js
+++ rpg.js
@@ -29,19 +29,19 @@ let protagonist = new Character("Protagonist", 2, heal, fireball);
 let antagonist = new Character("Antagonist", 1, fireball);
 
 function fireball(target) {
-  amount = (this.level*2);
+  let amount = (this.level*2);
   target.health -= amount;
   return `${this.name} deals ${amount} damage to ${target.name}!`;
 }
 
 function heal(target) {
-  amount = (this.level*2);
+  let amount = (this.level*2);
   target.health += amount;
   return `${this.name} heals ${amount} life to ${target.name}!`;
 }
 
 function blizzard(){
-  amount = (this.level/2);
+  let amount = (this.level/2);
   Character.instances.health -= amount;
   Character.instances.forEach((instance, instance.health) => {
     return `${instance.name} damaged ${amount}, ${instance.health} life remaining!`);
